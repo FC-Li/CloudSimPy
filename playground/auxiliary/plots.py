28d9c6f48e5418f1d09f96adf38eac58f47a5909
ariskaratzikos <aris.karatzikos@gmail.com>
2024-09-12 11:50:03 +0300

    last changes

--- playground/auxiliary/plots.py
+++ playground/auxiliary/plots.py
@@ -1,42 +1,47 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import textwrap
 import argparse
 
 def plot_graphs_from_csv(file_path):
     # Read the CSV file into a pandas DataFrame
     data = pd.read_csv(file_path)
     
-    # Extract the System_specs column for labeling
-    system_specs = data['Number of episodes']
+    # Extract the Number of episodes column for labeling
+    number_of_episodes = data['Number of episodes']
     
     # Define colors for each position
     colors = ['green', 'red', 'blue']
     
-    # Iterate over each column (excluding the System_specs column) and plot the graphs
+    # Iterate over each column (excluding the Number of episodes column) and plot the graphs
     for column in data.columns[1:]:
         plt.figure(figsize=(14, 8))  # Increase figure size
-        bars = plt.bar(range(len(system_specs)), data[column], align='center')
+        bars = plt.bar(range(len(number_of_episodes)), data[column], align='center')
 
         # Set colors for bars based on their position
         for i, bar in enumerate(bars):
-            # if i == 0 or i == 1:
-            #     bar.set_color('grey')
-            bar.set_color(colors[(i) % 3])
+            if i < 2:
+                bar.set_color('grey')
+            else:
+                bar.set_color(colors[(i+1) % 3])
         
-        plt.title(f'{column}')
+        # Set title with larger font size
+        plt.title(f'{column}', fontsize=20)
 
+        # Set larger font size for x and y axis labels
+        plt.xlabel('Number of episodes', fontsize=16)
+        plt.ylabel('Values', fontsize=16)
+        
         # Wrap the labels to fit better
-        wrapped_labels = [label for label in system_specs]
-        plt.xticks(range(len(system_specs)), wrapped_labels, rotation=0, fontsize=10)
+        wrapped_labels = [label for label in number_of_episodes]
+        plt.xticks(range(len(number_of_episodes)), wrapped_labels, rotation=0, fontsize=14)
 
         # Ensure all bars and labels are shown
         plt.subplots_adjust(bottom=0.25, right=0.95, top=0.90)  # Adjust layout to make room for the labels
 
-        # Add legend
+        # Add legend with larger font size
         handles = [plt.Rectangle((0,0),1,1, color=color) for color in colors]
         labels = ['200 sec response time threshold', '300 sec response time threshold', '400 sec response time threshold']
-        plt.legend(handles, labels)
+        plt.legend(handles, labels, fontsize=14)
 
         plt.tight_layout()  # Adjust layout to make room for the labels
         plt.show()
